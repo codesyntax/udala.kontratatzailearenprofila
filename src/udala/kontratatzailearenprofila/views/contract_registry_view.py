6083587911b7aec5204df5f868b0c725856ab30f
Mikel Larreategi <mlarreategi@codesyntax.com>
2025-08-29 10:33:53 +0200

    feat: new widget

---
+++ src/udala/kontratatzailearenprofila/views/contract_registry_view.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# from udala.kontratatzailearenprofila import _
+from Products.Five.browser import BrowserView
+from zope.interface import implementer
+from zope.interface import Interface
+
+# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFPlone.utils import get_installer
+from plone import api
+class IContractRegistryView(Interface):
+    """ Marker Interface for IContractRegistryView"""
+
+
+@implementer(IContractRegistryView)
+class ContractRegistryView(BrowserView):
+    # If you want to define a template here, please remove the template from
+    # the configure.zcml registration of this view.
+    # template = ViewPageTemplateFile('contractors_profile_view.pt')
+
+    def __call__(self):
+        # Implement your own actions:
+        return self.index()
+
+
+    def language(self):
+        portal = api.portal.get()
+        installer = get_installer(portal, self.request)
+        if installer.is_product_installed('plone.app.multilingual'):
+            return ""
+
+        return api.portal.get_current_language()
